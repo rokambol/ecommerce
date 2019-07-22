74c6627d0a6a658664499a12fe2666a7eeea0889
Ubuntu <ubuntu@ip-172-31-93-57.ec2.internal>
2019-07-22 20:00:47 +0000

    update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
+# import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -90,7 +90,17 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 #    }
 #}
 
-DATABASES ={ 'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+
+if "DATABASE_URL" in os.environ:
+    DATABASES ={ 'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+else:
+    print("DATABASE URL NOT FOUND. USING sqlite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
